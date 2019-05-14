b0a1ac9985ea22c6c061d2b70ab06969399e02fe
yurkiss <terranet23@gmail.com>
2019-05-15 00:11:00 +0300

    add EF, migrations

---
+++ simpliChat/Entities/EFContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace simpliChat.Entities
+{
+    public class EFContext : DbContext
+    {
+        public EFContext() : base("conStr")
+        {
+
+        }
+    }
+}
---
+++ simpliChat/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace simpliChat.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<simpliChat.Entities.EFContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(simpliChat.Entities.EFContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
